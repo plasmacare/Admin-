444b4b33ce1d222b4cea8fce75b283ded144feb1
plasmacare <plasmacaretechnicaluse@gmail.com>
2026-08-06 13:39:55 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AuthProvider } from './lib/auth.jsx'
+import './styles/tokens.css'
+import './styles/global.css'
+import './styles/admin.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+)
